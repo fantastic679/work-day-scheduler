8126c435417ec791b07d698ec970db388852a208
Steven Phan <stevenphan@msn.com>
2022-01-09 23:15:25 +1100

    Changed textarea colour based on time

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var todayString = today.format("Do") + " of " + today.format("MMMM") + ", " + to
 $("#currentDay").text(todayString);
 // var asdasd = document.getElementById("currentDay");
 // asdasd.textContent = todayString;
+
+$("[time24]").each(function() {
+    if (parseInt($(this).attr("time24")) < today.hours()) {
+        $(this).addClass("past");
+    } else if (parseInt($(this).attr("time24")) == today.hours()) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+});
